8e187c177f1e82562a7555955294b3d14116b58c
Rafael Rafael <rrafael@dip-badajoz.es>
2022-11-16 11:45:52 +0100

    20221115

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,26 +19,33 @@ class ProductSeeder extends Seeder
         DB::table('products')->insert([
             'name'=>'Bocadillo de calamares',
             'price'=>'6',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
         DB::table('products')->insert([
             'name'=>'Bocadillo de panceta',
             'price'=>'6',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
         DB::table('products')->insert([
             'name'=>'Bocadillo de jamón ibérico',
             'price'=>'6',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
         DB::table('products')->insert([
             'name'=>'Bocadillo vegetal',
             'price'=>'6',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
         DB::table('products')->insert([
             'name'=>'Hamburguesa de vacuno',
             'price'=>'7',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
         DB::table('products')->insert([
             'name'=>'Hamburguesa mixta',
             'price'=>'5.5',
+            'file_path'=>'assets/img/bocatas/calamares.jpg',
         ]);
     }
+}
 }
